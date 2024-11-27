1885e6abfa1f8921daabf11c1135623abfc0eb8b
Geo <georgeyoussefrady@gmail.com>
2024-11-27 11:33:04 +0200

    initial

---
+++ lib/style/AppStyle.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppStyle{
+static ThemeData lightTheme = ThemeData(
+
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+);
+}
